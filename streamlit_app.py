af328fb947d7d6ca17c302c7cdba2eff031d24a3
QMR <46519498+qmrcat@users.noreply.github.com>
2025-12-11 12:31:17 +0000

    Modificacions menors

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,14 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("🎈 La meva primera aplicaccio")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+
+quantitat = st.slider("Selecciona la quantitat")
+
+st.write(f'La quantitat seleccionada es: {quantitat}')
+
+for i in range(quantitat):
+    st.button(f'{i+1}')
